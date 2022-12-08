5fc083675777950d4b2a4730d70764f0f213a6cf
blueboi <blueboi@blueboi>
2022-12-08 14:45:03 +0100

    first commit

---
+++ client/src/components/News.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function News() {
+  return (
+    <article className="container">
+      <div>
+        <h2>To do</h2>
+        <p>Update form</p>
+        <p>Audio Player</p>
+        <p>Auth Email</p>
+        <p>Default Cover</p>
+        <p>Fix Responsive</p>
+        <p>Convert MP3/test</p>
+        <p>Control Upload</p>
+        <p>yoeeeeeo</p>
+        <p>SMTP Hebergeur</p>
+      </div>
+    </article>
+  );
+}
+
+export default News;
